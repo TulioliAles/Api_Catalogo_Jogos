e6f2d4fa9eeb96f5a75e5bde0703c09bf104939c
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-08-04 21:46:18 -0300

    Implementa Endpoints da JogoController

--- ApiCatalogoJogos/Controllers/V1/JogosController.cs
+++ ApiCatalogoJogos/Controllers/V1/JogosController.cs
@@ -11,6 +11,40 @@ namespace ApiCatalogoJogos.Controllers.V1
     [ApiController]
     public class JogosController : ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> GetListaJogo()
+        {
+            return Ok();
+        }
 
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<object>> GetJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirJogo(object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarPrecoJogo(Guid idJogo, double preco)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<ActionResult> ApagarJogo(Guid idJogo)
+        {
+            return Ok();
+        }
     }
 }
